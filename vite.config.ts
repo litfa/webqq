083068836de5b85c747375fe4087daee4c440d4c
ltfei <ltfei@ltfei.cn>
2023-03-16 20:03:30 +0800

    feat: px to vw

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import pptv from 'postcss-px-to-viewport-8-plugin'
+
+const load_pptv = pptv({
+  viewportWidth: 375, // 设计稿的视口宽度,如传入函数
+  propList: ['*'],
+  unitPrecision: 6, // 转换后的精度，即小数点位数
+  minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+  replace: true // 是否转换后直接更换属性值
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [load_pptv]
+    }
+  }
 })
