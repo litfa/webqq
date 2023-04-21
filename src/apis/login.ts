a1345f278ec1875babc40239fe0b66c32e9eead3
ltfei <ltfei@ltfei.cn>
2023-04-21 21:45:48 +0800

    feat: 登录验证码

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -1,11 +1,23 @@
 import request, { Response } from '../utils/request'
 
-export const login = (password: string) => {
+export const login = (
+  password: string,
+  verificationCodeId: string,
+  verificationCode: string
+) => {
   return request({
     url: '/login/',
     method: 'post',
     data: {
-      key: password
+      key: password,
+      verificationCodeId,
+      verificationCode
     }
+  })
+}
+export const getCode = (): Response<{ svg: string; id: string }> => {
+  return request({
+    url: '/login/getCode',
+    method: 'post'
   })
 }
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { login as loginApi } from '../apis/login'
+import { login as loginApi, getCode as getCodeApi } from '../apis/login'
 
 const password = ref('')
+const code = ref('')
+let codeid = ''
+const svg = ref('')
 const login = () => {
-  loginApi(password.value)
+  loginApi(password.value, codeid, code.value)
 }
+
+const getCode = async () => {
+  const { data: res } = await getCodeApi()
+  codeid = res.data.id
+  svg.value = res.data.svg
+}
+getCode()
 </script>
 
 <template>
-  <input type="text" v-model="password" />
+  <input type="text" v-model="password" placeholder="密码" />
+  <input type="text" v-model="code" placeholder="验证码" />
+  <div class="svg" v-html="svg"></div>
   <button @click="login">登录</button>
 </template>
 
