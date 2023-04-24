13deb423344685bb9d42d504f17f467eec9d2b89
ltfei <ltfei@ltfei.cn>
2023-04-24 16:20:39 +0800

    feat: element

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import pptv from 'postcss-px-to-viewport-8-plugin'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 const load_pptv = pptv({
   viewportWidth: 375, // 设计稿的视口宽度,如传入函数
@@ -13,7 +16,15 @@ const load_pptv = pptv({
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   css: {
     postcss: {
       plugins: [load_pptv]
