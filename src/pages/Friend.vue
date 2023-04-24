1ea86f2ca36600975045afc58395517615e48898
ltfei <ltfei@ltfei.cn>
2023-04-24 15:41:12 +0800

    feat: 分离功能

--- src/pages/Friend.vue
+++ src/pages/Friend.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import Bubble from '../components/Bubble/Bubble.vue'
 import { getFriendMessageRecord } from '../apis/getMessageRecord'
 import { useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
 import { FriendMessageType, FriendSyncMessageType } from '../types/Message'
-import { messageTime } from '../utils/dayjs'
+import Chatlist from '../components/Chatlist/Chatlist.vue'
 
 const route = useRoute()
 const chat = ref<null | HTMLDivElement>(null)
@@ -30,36 +29,6 @@ const init = async () => {
   el.scrollTop = el.scrollHeight
 }
 init()
-
-const display = (
-  i: number,
-  e: FriendMessageType | FriendSyncMessageType
-): { info: boolean; date: string | null } => {
-  if (i == 0) {
-    return { info: true, date: null }
-  }
-
-  const lastMessage = list.value[i - 1]
-
-  const date = messageTime(lastMessage.timestamp, e.timestamp)
-
-  if (date) {
-    return { info: true, date }
-  }
-
-  if (lastMessage.type == e.type) {
-    if (lastMessage.type == 'FriendMessage' && e.type == 'FriendMessage') {
-      if (lastMessage.sender.id == e.sender.id) {
-        return { info: false, date: null }
-      }
-    } else if (lastMessage.type == 'FriendSyncMessage' && e.type == 'FriendSyncMessage') {
-      // sync都是自己的消息，可以直接隐藏
-      return { info: false, date: null }
-    }
-  }
-
-  return { info: true, date: null }
-}
 
 const load = async () => {
   await getMessageRecord(list.value.at(0)?.id, list.value.at(0)?.timestamp)
@@ -87,24 +56,7 @@ onMounted(() => {
 
 <template>
   <div class="chat" ref="chat">
-    <template v-for="(i, index) in list">
-      <div class="date" v-if="display(index, i).date">
-        {{ display(index, i).date }}
-      </div>
-      <bubble
-        v-if="i.type == 'FriendMessage'"
-        :qq="i.sender.id"
-        :message-chain="i.messageChain"
-        :name="i.sender.remark || i.sender.nickname"
-        :info-display="display(index, i).info"
-      />
-      <bubble
-        v-if="i.type == 'FriendSyncMessage'"
-        :message-chain="i.messageChain"
-        :avatar-display="'hidden'"
-        reverse
-      />
-    </template>
+    <chatlist :list="list" />
   </div>
 </template>
 
@@ -114,9 +66,6 @@ onMounted(() => {
   overflow: auto;
   display: flex;
   flex-direction: column;
-  .date {
-    text-align: center;
-    margin: 15px 0;
-  }
+
 }
 </style>
--- src/pages/Group.vue
+++ src/pages/Group.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import Bubble from '../components/Bubble/Bubble.vue'
 import { getGroupRecord } from '../apis/getMessageRecord'
 import { useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
 import { GroupMessageType, GroupSyncMessageType } from '../types/Message'
-import { messageTime } from '../utils/dayjs'
+import Chatlist from '../components/Chatlist/Chatlist.vue'
 
 const route = useRoute()
 const chat = ref<null | HTMLDivElement>(null)
@@ -32,36 +31,6 @@ const init = async () => {
 }
 init()
 
-const display = (
-  i: number,
-  e: GroupMessageType | GroupSyncMessageType
-): { info: boolean; date: string | null } => {
-  if (i == 0) {
-    return { info: true, date: null }
-  }
-
-  const lastMessage = list.value[i - 1]
-
-  const date = messageTime(lastMessage.timestamp, e.timestamp)
-
-  if (date) {
-    return { info: true, date }
-  }
-
-  if (lastMessage.type == e.type) {
-    if (lastMessage.type == 'GroupMessage' && e.type == 'GroupMessage') {
-      if (lastMessage.sender.id == e.sender.id) {
-        return { info: false, date: null }
-      }
-    } else if (lastMessage.type == 'GroupSyncMessage' && e.type == 'GroupSyncMessage') {
-      // sync都是自己的消息，可以直接隐藏
-      return { info: false, date: null }
-    }
-  }
-
-  return { info: true, date: null }
-}
-
 const load = async () => {
   await getMessageRecord(list.value.at(0)?.id, list.value.at(0)?.timestamp)
 }
@@ -88,26 +57,7 @@ onMounted(() => {
 
 <template>
   <div class="chat" ref="chat">
-    <template v-for="(i, index) in list">
-      <div class="date" v-if="display(index, i).date">
-        {{ display(index, i).date }}
-      </div>
-      <bubble
-        v-if="i.type == 'GroupMessage'"
-        :qq="i.sender.id"
-        :message-chain="i.messageChain"
-        :name="i.sender.memberName"
-        :info-display="display(index, i).info"
-      />
-      <bubble
-        v-if="i.type == 'GroupSyncMessage'"
-        :message-chain="i.messageChain"
-        name="XING"
-        :avatar-display="'hidden'"
-        :info-display="display(index, i).info"
-        reverse
-      />
-    </template>
+    <chatlist :list="list" />
   </div>
 </template>
 
@@ -117,9 +67,5 @@ onMounted(() => {
   overflow: auto;
   display: flex;
   flex-direction: column;
-  .date {
-    text-align: center;
-    margin: 15px 0;
-  }
 }
 </style>
